9c41e3b88e23806ab57ec6c541cfc22e14342532
kirby00001 <t3273139612@hotmail.com>
2023-03-18 23:24:21 +0800

    (N,H,W,C ) to (N,C,H,W)

--- datasets/IDRiD.py
+++ datasets/IDRiD.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from glob import glob
 from torch.utils.data import Dataset, DataLoader
 from utils.utils import load_image, path2paths, load_mask
@@ -14,24 +15,29 @@ class IDRiD(Dataset):
 
     def __getitem__(self, index):
         img_path = self.img_paths[index]
-        img = load_image(img_path)
-        mask = load_mask(path2paths(img_path))
+        img = load_image(img_path).transpose(2, 0, 1)
+        mask = load_mask(path2paths(img_path)).transpose(2, 0, 1)
         return torch.Tensor(img), torch.Tensor(mask)
 
 
-def get_dataloader_IDRiD(batch_size):
+def get_dataloader_IDRiD(batch_size=1, shuffle=True):
     img_paths = glob(
-        "./data/IDRiD/A. Segmentation1. Original Images/a. Training Set/*.jpg"
+        "./data/IDRiD/A. Segmentation/1. Original Images/a. Training Set/*.jpg"
     )
     img_paths.sort()
+    # print("image paths:")
+    # for img_path in img_paths:
+    #     print(img_path)
     dataset = IDRiD(img_paths=img_paths)
     return DataLoader(
         dataset=dataset,
         batch_size=batch_size,
-        drop_last=False,
+        shuffle=shuffle,
     )
 
 
 if __name__ == "__main__":
-    dataloader = get_dataloader_IDRiD(1)
-    print(type(dataloader))
+    dataloader = get_dataloader_IDRiD(batch_size=1)
+    img, mask = next(iter(dataloader))
+    print("img.shape:", img.shape)
+    print("mask.shape:", mask.shape)
--- utils/utils.py
+++ utils/utils.py
@@ -3,9 +3,22 @@ import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def load_image(path):
     img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
     return img
+
+
+# data/IDRiD/A. Segmentation/2. All Segmentation Groundtruths/b. Testing Set/1. Microaneurysms
+def path2paths(image_path):
+    infos = image_path.split("/")
+    train_or_test = infos[-2]
+    # print("train_or_test:", train_or_test)
+    i = infos[-1][:-4]
+    # print("ID:",i)
+    return glob.glob(
+        f"./data/IDRiD/A. Segmentation/2. All Segmentation Groundtruths/{train_or_test}/[1234]*/{i}_*.tif"
+    )
 
 
 def path2id(path):
@@ -15,29 +28,26 @@ def path2id(path):
 def load_mask(paths):
     masks = np.zeros(shape=(2848, 4288, 4))
     for path in paths:
+        # print("path2id:",path2id(path))
         masks[:, :, path2id(path) - 1] = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-    return masks / 255.0
-
-
-# data/IDRiD/A. Segmentation/2. All Segmentation Groundtruths/b. Testing Set/1. Microaneurysms
-def path2paths(image_path):
-    infos = image_path.split("/")
-    train_or_test = infos[-2]
-    print(train_or_test)
-    i = infos[-1][:-4]
-    print(i)
-    return glob.glob(
-        f"./data/IDRiD/A. Segmentation/2. All Segmentation Groundtruths/{train_or_test}/[1234]*/{i}_*.tif"
-    )
+    return masks
 
 
 if __name__ == "__main__":
+
     img_path = (
-        "./data/IDRiD/A. Segmentation/1. Original Images/a. Training Set/IDRiD_01.jpg"
+        "./data/IDRiD/A. Segmentation/1. Original Images/a. Training Set/IDRiD_10.jpg"
     )
+    # load image
     img = load_image(img_path)
-    print(img)
-    mask_paths=path2paths(img_path)
-    print(mask_paths)
-    mask=load_mask(mask_paths)
-    print(mask.shape)
+    print("img.max:", img.max())
+    print("img.shape", img.shape)
+    # from image path to mask paths
+    mask_paths = path2paths(img_path)
+    print("mask paths:")
+    for mask_path in mask_paths:
+        print(mask_path)
+    # load masks
+    mask = load_mask(mask_paths)
+    print("mask.max", mask.max())
+    print("mask.shape", mask.shape)

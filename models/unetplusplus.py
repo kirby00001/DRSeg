91701cea075181a44b0ad62a0ef2786d712a46a2
kirby00001 <t3273139612@hotmail.com>
2023-03-18 23:24:49 +0800

    Unet++

--- models/unetplusplus.py
+++ models/unetplusplus.py
@@ -1,7 +1,15 @@
 import segmentation_models_pytorch as smp
 
 
-def get_unetpp(encoder_name, encoder_weights, classes):
+def get_unetpp(
+    encoder_name,
+    encoder_weights,
+    classes=4,
+    encoder_depth=4,
+    decoder_channels=[32, 96, 144, 240],
+    in_channels=3,
+    decoder_use_batchnorm=True,
+):
     """_summary_
 
     Args:
@@ -9,8 +17,20 @@ def get_unetpp(encoder_name, encoder_weights, classes):
         encoder_weights (_type_): _description_
         classes (_type_): _description_
     """
-    smp.UnetPlusPlus(
+    return smp.UnetPlusPlus(
         encoder_name=encoder_name,
         encoder_weights=encoder_weights,
+        encoder_depth=encoder_depth,
+        decoder_channels=decoder_channels,
         classes=classes,
+        in_channels=in_channels,
+        decoder_use_batchnorm=decoder_use_batchnorm,
     )
+
+
+if __name__ == "__main__":
+    model = get_unetpp(
+        encoder_name="efficientnet-b0",
+        encoder_weights="imagenet",
+    )
+    print(model)

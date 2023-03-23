19f4a853618d2bdce14940fea595183942fce1cf
kirby00001 <t3273139612@hotmail.com>
2023-03-23 14:57:33 +0800

    mask2label

--- utils/load.py
+++ utils/load.py
@@ -1,10 +1,12 @@
 import cv2
 import glob
+from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def load_image(path):
-    img = cv2.imread(path, cv2.IMREAD_COLOR)
+    img = np.array(Image.open(path))
     return img
 
 
@@ -16,7 +18,7 @@ def path2paths(image_path):
     i = infos[-1][:-4]
     # print("ID:",i)
     return glob.glob(
-        f"./data/IDRiD/A. Segmentation/2. All Segmentation Groundtruths/{train_or_test}/[1234]*/{i}_*.tif"
+        f"../data/IDRiD/A. Segmentation/2. All Segmentation Groundtruths/{train_or_test}/[1234]*/{i}_*.tif"
     )
 
 
@@ -30,29 +32,48 @@ def load_mask(paths):
         # print("path2id:",path2id(path))
         image_id = path2id(path)
         mask = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        print(f"mask{image_id}.max", mask.max())
         masks[:, :, image_id - 1] = mask
-    return masks/76.0
+    return masks / 76.0
+
+
+def mask2label(masks):
+    pad_masks = np.pad(masks, pad_width=[(0, 0), (0, 0), (1, 0)])
+    label = np.argmax(pad_masks, axis=-1)
+    return label
 
 
 if __name__ == "__main__":
-
-    mask_path = "./data/IDRiD/A. Segmentation/2. All Segmentation Groundtruths/b. Testing Set/3. Hard Exudates/IDRiD_81_EX.tif"
-    mask = cv2.imread(mask_path, cv2.IMREAD_REDUCED_GRAYSCALE_8)
-    print(mask.max())
-    
     img_path = (
-        "./data/IDRiD/A. Segmentation/1. Original Images/b. Testing Set/IDRiD_81.jpg"
+        "../data/IDRiD/A. Segmentation/1. Original Images/a. Training Set/IDRiD_17.jpg"
+    )
+    img_path = (
+        "../data/IDRiD/A. Segmentation/1. Original Images/b. Testing Set/IDRiD_81.jpg"
     )
     # load image
     img = load_image(img_path)
     print("img.max:", img.max())
     print("img.shape", img.shape)
+    # plt.imshow(img)
     # from image path to mask paths
     mask_paths = path2paths(img_path)
     print("mask paths:")
     for mask_path in mask_paths:
         print(mask_path)
     # load masks
-    mask = load_mask(mask_paths)
-    print("mask.max", mask.max())
-    print("mask.shape", mask.shape)
+    masks = load_mask(mask_paths)
+    print("mask.max", masks.max())
+    print("mask.shape", masks.shape)
+
+    # Visualization
+    # cmaps = ["Greens_r", "Purples_r", "Blues_r", "Reds_r"]
+    # masks = np.ma.masked_where(masks == 0, masks)
+    # print(masks)
+    # for i in range(4):
+    #     plt.imshow(masks[:, :, i], alpha=1, cmap=cmaps[i])
+
+    # to label
+    label = mask2label(masks)
+    print(label)
+    print("label.shape", label.shape)
+    print("label.max", label.max())
--- utils/loss.py
+++ utils/loss.py
@@ -1,5 +1,5 @@
+import torch
 from torch.nn import CrossEntropyLoss
-
 
 def get_loss_CE(weight=None):
     return CrossEntropyLoss(
@@ -8,5 +8,7 @@ def get_loss_CE(weight=None):
 
 
 if __name__ == "__main__":
-    loss = get_loss_CE()
-    print(loss)
+    loss_fn = get_loss_CE()
+    input = torch.rand(size=(1, 5, 480, 720))
+    target = torch.randint(low=0, high=1, size=(1, 5, 480, 720))
+    print(loss_fn(input, target))
